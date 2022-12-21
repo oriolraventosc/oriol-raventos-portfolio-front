04b49f22cda2a387cb901f405e40aced9a1d2058
oriolraventosc <oriol.raventos.c@gmail.com>
2022-12-21 16:31:49 +0100

    Add aria labels

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,6 +16,7 @@ const Footer = () => {
         <a
           href="https://github.com/oriolraventosc"
           alt="oriol raventós github profile"
+          aria-label="oriol raventós github profile"
           target="_blank"
           rel="noreferrer"
         >
@@ -29,6 +30,7 @@ const Footer = () => {
         <a
           href="https://www.linkedin.com/in/oriolraventosc/"
           alt="oriol raventós linkedin profile"
+          aria-label="oriol raventós linkedin profile"
           target="_blank"
           rel="noreferrer"
         >
@@ -42,6 +44,7 @@ const Footer = () => {
         <a
           href="mailto: oriol.raventos.c@gmail.com"
           alt="oriol raventós e-mail"
+          aria-label="oriol raventós email"
         >
           <EmailIcon
             className="icon"
@@ -53,6 +56,7 @@ const Footer = () => {
         <a
           href="https://www.instagram.com/oraventosc/"
           alt="oriol raventós instagram profile"
+          aria-label="oriol raventós instagram profile"
           target="_blank"
           rel="noreferrer"
         >
@@ -66,6 +70,7 @@ const Footer = () => {
         <a
           href="https://twitter.com/oraventosc"
           alt="oriol raventós twitter profile"
+          aria-label="oriol raventós twitter profile"
           target="_blank"
           rel="noreferrer"
         >
@@ -76,7 +81,11 @@ const Footer = () => {
             }}
           />
         </a>
-        <a href="tel: 664622830" alt="oriol raventós telephone number">
+        <a
+          href="tel: 664622830"
+          alt="oriol raventós telephone number"
+          aria-label="oriol raventós telephone number"
+        >
           <CallIcon
             className="icon"
             sx={{
