8b2aefaf1014f0e7df34ab2f6f4ac60a6d47018a
oriolraventosc <oriol.raventos.c@gmail.com>
2024-02-22 11:19:02 +0100

    Fix unused variable

--- src/pages/Blogpage/Blogpage.jsx
+++ src/pages/Blogpage/Blogpage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import BlogpageStyled from "./BlogpageStyled";
